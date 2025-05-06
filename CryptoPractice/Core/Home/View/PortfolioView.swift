0f49f111cf8ea3df1f2dd36da0a92da9560dffaf
Yen Lin <e19970722@gmail.com>
2025-05-06 22:27:31 +0800

    add Portfolio Core Data

--- CryptoPractice/Core/Home/View/PortfolioView.swift
+++ CryptoPractice/Core/Home/View/PortfolioView.swift
@@ -34,7 +34,7 @@ struct PortfolioView: View {
                     XMarkButton(isPresented: $isPresented)
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    
+                    trailingNavigationButton
                 }
             })
             .onChange(of: vm.searchText) { _, newValue in
@@ -56,13 +56,13 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack(spacing: 10) {
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 75)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                                selectedCoin = coin
+                                updateSelectedCoin(coin: coin)
                             }
                         }
                         .background(
@@ -74,6 +74,16 @@ extension PortfolioView {
             }
             .frame(height: 120)
             .padding(.leading)
+        }
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+        if let portfolioCoin = vm.portfolioCoins.first(where: { $0.id == selectedCoin?.id }),
+           let amount = portfolioCoin.currentHoldings {
+            quantityText = "\(amount)"
+        } else {
+            quantityText = ""
         }
     }
     
@@ -128,9 +138,11 @@ extension PortfolioView {
     
     private func saveButtonPressed() {
         
-        guard let coin = selectedCoin else { return }
+        guard let coin = selectedCoin,
+              let amount = Double(quantityText) else { return }
         
         // save to portfolio
+        vm.updatePortfolio(coin: coin, amount: amount)
         
         // show checkmark
         withAnimation(.easeIn) {
--- CryptoPractice/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoPractice/Core/Home/ViewModel/HomeViewModel.swift
@@ -13,11 +13,12 @@ class HomeViewModel: ObservableObject {
     @Published var statistics: [StatisticModel] = []
     
     @Published var allCoins: [CoinModel] = []
-    @Published var portfolioCoin: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
+    private let portfolioDataService = PortfolioDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -42,6 +43,25 @@ class HomeViewModel: ObservableObject {
                 self?.statistics = returnedStats
             }
             .store(in: &cancellables)
+        
+        // update Portoflio
+        $allCoins
+            .combineLatest(portfolioDataService.$saveEntities)
+            .map { (coinModels, portfolioEntities) -> [CoinModel] in
+                coinModels.compactMap { (coin) -> CoinModel? in
+                    guard let entity = portfolioEntities.first(where: { $0.coinID == coin.id }) else {
+                        return nil
+                    }
+                    return coin.updateHoldings(amount: entity.amount)
+                }
+            }.sink { (returnedCoin) in
+                self.portfolioCoins = returnedCoin
+            }
+            .store(in: &cancellables)
+    }
+    
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+        portfolioDataService.updatePortfolio(coin: coin, amount: amount)
     }
     
     func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
